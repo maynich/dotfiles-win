6bf5cd5480a7ca9b5dd37e3874b8482a15b818fb
szukalski <szukalski@gmail.com>
2017-10-19 15:12:45 +0200

    zoo

--- install.sh
+++ install.sh
@@ -36,5 +36,8 @@ ln -sf $(pwd)/.bashrc ~/.bashrc
 wget https://github.com/szukalski/.vim/raw/master/setup.sh -O ~/vim-setup.sh
 chmod +x ~/vim-setup.sh
 
+# pandoc
+sudo apt -qq install pandoc
+
 # Fin
 echo "Setup your ssh private keys, restart your shell login, then run ~/vim-setup.sh"
